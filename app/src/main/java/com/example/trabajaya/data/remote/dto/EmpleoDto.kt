c0d15fb8c06c372ef4ef6e5d47249f434ce8d189
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-11-12 23:27:22 -0400

    EmpleoDto added.

---
+++ app/src/main/java/com/example/trabajaya/data/remote/dto/EmpleoDto.kt
@@ -0,0 +1,14 @@
+package com.example.trabajaya.data.remote.dto
+
+import androidx.room.PrimaryKey
+
+data class EmpleoDto(
+    @PrimaryKey var empleoId: Int? = null,
+    var nombre: String ="",
+    var descripcion: String ="",
+    var categoria: String ="",
+    var fechaDePublicacion: String = "",
+    var numero: String = "",
+    var correo: String = "",
+    var direccion: String = "",
+)
