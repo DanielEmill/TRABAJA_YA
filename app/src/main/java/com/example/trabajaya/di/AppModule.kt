ad96e1c108c90a71dc5ee47c0a093354ce0a8f7a
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-11-13 11:38:25 -0400

    Update AppModule ProvidesEmpleoApi Added.

--- app/src/main/java/com/example/trabajaya/di/AppModule.kt
+++ app/src/main/java/com/example/trabajaya/di/AppModule.kt
@@ -1,11 +1,14 @@
 package com.example.trabajaya.di
 
+import com.example.trabajaya.data.remote.dto.EmpleoApi
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,5 +20,14 @@ object AppModule {
         return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
     }
 
+    @Singleton
+    @Provides
+    fun providesEmpleoApi(moshi: Moshi): EmpleoApi {
+        return Retrofit.Builder()
+            .baseUrl("http://miempleoapiap.somee.com/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(EmpleoApi::class.java)
+    }
 
 }
