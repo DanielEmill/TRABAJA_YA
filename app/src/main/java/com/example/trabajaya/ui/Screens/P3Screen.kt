a4d8e6b1f1d962f47dd895f3453adf8178c93d5d
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-11-25 15:57:18 -0400

    ENVIATU CV, PANTALLA CREADA.

--- app/src/main/java/com/example/trabajaya/ui/Screens/P3Screen.kt
+++ app/src/main/java/com/example/trabajaya/ui/Screens/P3Screen.kt
@@ -1,10 +1,18 @@
 package com.example.trabajaya.ui.Screens
 
+import android.net.Uri
 import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,45 +20,99 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.trabajaya.ui.viewModel.EmpleoViewModel
 import com.example.trabajaya.utils.directionModule.ScreenDirectionModule
 import kotlinx.coroutines.launch
 
-
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun P3Screen(navController: NavController) {
-    val scope =  rememberCoroutineScope()
+fun P3Screen(
+    navController: NavController, empleoViewModel: EmpleoViewModel = hiltViewModel()
+) {
+    val scope = rememberCoroutineScope()
     Column(
-        modifier = Modifier
-            .size(200.dp, 200.dp),
-        horizontalAlignment = Alignment.Start
+        modifier = Modifier.size(200.dp, 200.dp), horizontalAlignment = Alignment.Start
     ) {
-        Spacer(modifier = Modifier.height(10.dp))
-        Icon(
-            imageVector = Icons.Filled.ArrowBack,
-            contentDescription = null,
+        Spacer(modifier = Modifier.height(20.dp))
+        Row(
             modifier = Modifier
-                .size(50.dp, 50.dp)
-                .padding(4.dp)
-                .clickable {
-                    scope.launch { navController.navigate(ScreenDirectionModule.PMain.route) }
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(imageVector = Icons.Filled.ArrowBack,
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(4.dp)
+                    .clickable {
+                        scope.launch { navController.navigate(ScreenDirectionModule.PMain.route) }
+                    })
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val context = LocalContext.current
+            val launcher =
+                rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
                 }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Sube tu CV",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                Text(
+                    text = "Nosotros te buscamos el trabajo",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(bottom = 32.dp)
+                )
+
+                UploadButton(onClick = {
+                    launcher.launch("application/pdf")
+                })
+            }
+        }
+    }
+}
+
+@Composable
+fun UploadButton(onClick: () -> Unit) {
+    IconButton(
+        onClick = onClick,
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(16.dp)
+            .size(56.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Default.Send,
+            contentDescription = "Subir CV",
+            tint = MaterialTheme.colorScheme.onPrimary
         )
-        Spacer(modifier = Modifier.padding(200.dp))
-        Text(text = "Pantalla #3", fontSize = 40.sp, modifier = Modifier
-            .fillMaxWidth()
-            .padding(7.dp)
-            .wrapContentWidth(
-                Alignment.CenterHorizontally
-            ))
     }
 }
